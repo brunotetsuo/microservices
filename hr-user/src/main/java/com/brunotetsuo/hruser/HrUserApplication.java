2397d7e5eb2055b931774d8e161128908c5ada80
Bruno Tetsuo <mundoct@hotmail.com>
2021-01-22 17:46:36 -0300

    Project hr-user created

---
+++ hr-user/src/main/java/com/brunotetsuo/hruser/HrUserApplication.java
@@ -0,0 +1,13 @@
+package com.brunotetsuo.hruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+}
